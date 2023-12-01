d423d7e38ef4ae5777c36dcb8c1174ca222b71ff
khalilsellamii <khalilsellami804@gmail.com>
2023-12-01 23:12:14 +0100

    cmnt

--- sonarqube_azure_vm_TF/main.tf
+++ sonarqube_azure_vm_TF/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  skip_provider_registration = true 
   features {}
 }
 
